e53bf0ee8a4f7bc872374b499aa810b6630bd33e
Josan <josanllomo@gmail.com>
2021-10-24 18:17:46 +0200

    Cambiada asignación de eventos para window.load y button.click

--- Primera Pagina/js/index.js
+++ Primera Pagina/js/index.js
@@ -1,36 +1,37 @@
-window.onload=inicio;
+window.addEventListener('load', inicio);
 
 var boton;
 var mp3;
 
-function inicio(){
+function inicio() {
 
-    document.getElementById("botonSonido").onclick=cambiarBoton;
+    document.getElementById("botonSonido").addEventListener('click', cambiarBoton);
+
 }
 
-function cambiarBoton(){
-    
+function cambiarBoton() {
+
     setTimeout(cambiarEstilo, 500);
     sonar();
 
 }
 
-function cambiarEstilo(){
-    
-    document.getElementById("botonSonido").textContent="GAME OVER";
-    boton=document.getElementById("botonSonido").style;
-    boton.padding="2em";
-    boton.backgroundColor="black";
-    boton.color="red";
-    boton.fontSize="xx-large";
-    boton.fontFamily="monospace";
-    boton.border="goldenrod 1em groove";
+function cambiarEstilo() {
+
+    document.getElementById("botonSonido").textContent = "GAME OVER";
+    boton = document.getElementById("botonSonido").style;
+    boton.padding = "2em";
+    boton.backgroundColor = "black";
+    boton.color = "red";
+    boton.fontSize = "xx-large";
+    boton.fontFamily = "monospace";
+    boton.border = "goldenrod 1em groove";
 
 }
 
-function sonar(){
-    
-    mp3=document.getElementById("sonido");
+function sonar() {
+
+    mp3 = document.getElementById("sonido");
     mp3.play();
 
 }
